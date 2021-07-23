4f8efa6664d7b66c22fd7eae5f1ddb4d8d7e5f62
sidesai2001 <sidesai2001@gmail.com>
2021-07-23 19:48:31 +0530

    changes

--- ExamTimetableApp/AdminLoginForm.Designer.cs
+++ ExamTimetableApp/AdminLoginForm.Designer.cs
@@ -45,16 +45,18 @@ namespace ExamTimetableApp
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.username);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(427, 208);
+            this.panel1.Size = new System.Drawing.Size(320, 169);
             this.panel1.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(141, 148);
+            this.button1.Location = new System.Drawing.Point(106, 120);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(241, 38);
+            this.button1.Size = new System.Drawing.Size(181, 31);
             this.button1.TabIndex = 2;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,43 +64,49 @@ namespace ExamTimetableApp
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(141, 95);
+            this.password.Location = new System.Drawing.Point(106, 77);
+            this.password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.password.Name = "password";
             this.password.PasswordChar = '*';
-            this.password.Size = new System.Drawing.Size(241, 22);
+            this.password.Size = new System.Drawing.Size(182, 20);
             this.password.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 40);
+            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username :";
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(141, 37);
+            this.username.Location = new System.Drawing.Point(106, 30);
+            this.username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(241, 22);
+            this.username.Size = new System.Drawing.Size(182, 20);
             this.username.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 98);
+            this.label2.Location = new System.Drawing.Point(31, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password :";
             // 
             // AdminLoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 248);
+            this.ClientSize = new System.Drawing.Size(346, 202);
             this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AdminLoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Administrator Login";
